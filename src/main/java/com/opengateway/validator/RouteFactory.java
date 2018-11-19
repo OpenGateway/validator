efda752637033189deaac2988a4848a8b950f138
dcryer <d.cryer@elsevier.com>
2018-11-19 23:38:20 +0000

    change endpoint from /simpleRoute to /post to match httpbin's endpoint for post requests and return 2xx and not 404

--- src/main/java/com/opengateway/validator/RouteFactory.java
+++ src/main/java/com/opengateway/validator/RouteFactory.java
@@ -2,6 +2,7 @@ package com.opengateway.validator;
 
 import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
@@ -11,9 +12,10 @@ class RouteFactory {
 
     Function<PredicateSpec, Route.AsyncBuilder> getSimpleroute() {
         return p -> p
-                .path("/simpleRoute")
+                .path("/post")
+                .and()
+                .method(HttpMethod.POST)
                 .filters(f -> f.filter(new OpenApiValidatorFilter().apply(new OpenApiValidatorFilter.OpenApiValidatorConfig())))
-
                 .uri("http://httpbin.org:80");
     }
 
