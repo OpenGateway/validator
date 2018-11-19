c9d9424da16774c749ef88fc412d8c585f7b0524
dcryer <d.cryer@elsevier.com>
2018-11-19 23:26:31 +0000

    separate unprocessable entity and ok entity flows - originally recognised error flow, but no return existed and therefore ok flow was executed too separate and refactor ok (request-rewrite) flow
    N.B. when request body is read, it appears necessary to rewrite it - some issue with Netty reading body later on causing two subscribers, where rewriting likely causes a read on the new body, not the old, fixing the problem (hack-ily, which I suppose this is all about...)

--- src/main/java/com/opengateway/validator/OpenApiValidatorFilter.java
+++ src/main/java/com/opengateway/validator/OpenApiValidatorFilter.java
@@ -6,6 +6,7 @@ import lombok.val;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.cloud.gateway.support.BodyInserterContext;
 import org.springframework.cloud.gateway.support.CachedBodyOutputMessage;
@@ -14,11 +15,12 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Nonnull;
 
 public class OpenApiValidatorFilter extends AbstractGatewayFilterFactory<OpenApiValidatorFilter.OpenApiValidatorConfig> {
     private static final Logger log = LoggerFactory.getLogger(OpenApiValidatorFilter.class);
@@ -29,7 +31,7 @@ public class OpenApiValidatorFilter extends AbstractGatewayFilterFactory<OpenApi
 
         return (exchange, chain) -> {
             val request = new DefaultServerRequest(exchange);
-            Mono<String> modifiedBody = request
+            return request
                     .bodyToMono(String.class)
                     .map(b -> new UnvalidatedRequest(request, b))
                     .flatMap(r -> {
@@ -39,51 +41,38 @@ public class OpenApiValidatorFilter extends AbstractGatewayFilterFactory<OpenApi
                             log.info("UNPROCESSABLE ENTITY");
                             val response = exchange.getResponse();
                             response.setStatusCode(HttpStatus.UNPROCESSABLE_ENTITY);
-                            response.setComplete();
+                            return response.setComplete();
                         }
                         log.info("ALL GOOD");
-                        return Mono.justOrEmpty(r.getBody());
+                        return rewriteRequest(exchange, chain, Mono.justOrEmpty(r.getBody()));
                     });
-            BodyInserter bodyInserter = BodyInserters.fromPublisher(modifiedBody, String.class);
-            HttpHeaders headers = new HttpHeaders();
-            headers.putAll(exchange.getRequest().getHeaders());
-
-
-            // if the body is changing content types, set it here, to the bodyInserter will know about it
-
-            CachedBodyOutputMessage outputMessage = new CachedBodyOutputMessage(exchange, headers);
-            return bodyInserter.insert(outputMessage,  new BodyInserterContext())
-                    // .log("modify_request", Level.INFO)
-                    .then(Mono.defer(() -> {
-                        ServerHttpRequestDecorator decorator = new ServerHttpRequestDecorator(
-                                exchange.getRequest()) {
-                            @Override
-                            public Flux<DataBuffer> getBody() {
-                                return outputMessage.getBody();
-                            }
-                        };
-                        return chain.filter(exchange.mutate().request(decorator).build());
-                    }));
         };
+    }
 
+    private Mono<Void> rewriteRequest(ServerWebExchange exchange, GatewayFilterChain chain, Mono<String> body) {
+        val headers = new HttpHeaders(exchange.getRequest().getHeaders());
+        val outputMessage = new CachedBodyOutputMessage(exchange, headers);
+        return BodyInserters.fromPublisher(body, String.class)
+                .insert(outputMessage,  new BodyInserterContext())
+                .then(Mono.defer(() -> {
+                    val decorator = new ServerHttpRequestDecorator(exchange.getRequest()) {
+                        @Nonnull
+                        @Override
+                        public Flux<DataBuffer> getBody() {
+                            return outputMessage.getBody();
+                        }
+                    };
+                    return chain.filter(exchange.mutate().request(decorator).build());
+                }));
     }
 
     public static class OpenApiValidatorConfig {
-
         final OpenApiInteractionValidator validator = OpenApiInteractionValidator
                 .createFor("/simple_route.yaml")
                 .build();
 
-
         public OpenApiInteractionValidator getValidator() {
             return validator;
-        }
-    }
-
-    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    public static class UnprocException extends RuntimeException {
-        public UnprocException(String message) {
-            super(message);
         }
     }
 }
